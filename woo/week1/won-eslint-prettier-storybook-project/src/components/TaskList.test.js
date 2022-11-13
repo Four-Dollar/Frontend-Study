24a38ae86d3e297b297dcb75a6e0fa1d4f2c16e9
wooyeonwon <dndus567@naver.com>
2022-11-13 16:04:42 +0900

    [스터디/원우연/1주차] prettier, eslint, storybook 적용한 리액트 프로젝트 실습

---
+++ woo/week1/won-eslint-prettier-storybook-project/src/components/TaskList.test.js
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+import * as TaskListStories from "./Task.stories";
+
+const { WithPinnedTasks } = composeStories(TaskListStories);
+
+if (
+  ("renders pinned tasks at the start of the list",
+  () => {
+    const { container } = render(<WithPinnedTasks />);
+
+    expect(
+      container.querySelector(
+        '.list-item:nth-child(1) input[value="Task 6 (pinned)"]'
+      )
+    ).not.toBe(null);
+  })
+);
